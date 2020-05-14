eceaa6283b1ce0428a94dafc5a058bc59dd48b49
maaush <mauribenitez95@gmail.com>
2020-05-13 23:50:37 -0300

    Finish part 1 theory, moving on with exercises.

---
+++ part1/exercises/unicafe/src/index.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
