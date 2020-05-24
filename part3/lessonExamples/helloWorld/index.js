e1239a9155e0d609bf5d7ce325ef06d30f0a1201
maaush <mauribenitez95@gmail.com>
2020-05-24 05:14:17 -0300

    Finish exercises related to Heroku and MongoDB. (3.9 through 3.12)

--- part3/lessonExamples/helloWorld/index.js
+++ part3/lessonExamples/helloWorld/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
-const PORT = 3001;
+const cors = require('cors');
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(cors());
 
 let notes = [
     {
